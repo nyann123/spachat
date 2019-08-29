3f71b0e0c3781db8300e8603834313f741b2911b
nyan <apple@gmail.com>
2019-08-29 10:52:09 +0900

    test

--- resources/js/router.js
+++ resources/js/router.js
@@ -63,10 +63,8 @@ router.beforeEach((to, from, next) => {
     const params = {  user_id : user.id,
                       room_id: to.params.id,
                      }
-                     console.log(params);
     axios.post(url, params)
       .then((response) => {
-        console.log(response);
       if(response.data){
         next();
       }else{
