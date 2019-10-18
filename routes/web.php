d3866722f1bfeb10801d375fcc00aeb2ed5d8fc1
nyan <apple@gmail.com>
2019-10-18 20:08:21 +0900

    remove Duplicate

--- routes/web.php
+++ routes/web.php
@@ -11,20 +11,20 @@
 |
 */
 
-Route::namespace('ajax')->group(function () {
-    Route::post('ajax/entry', 'UserEntry'); // ユーザー名登録
+Route::group(['prefix' => 'ajax' , 'namespace' => 'Ajax'] , function () {
+    Route::post('entry', 'UserEntry'); // ユーザー名登録
 
-    Route::get('ajax/chat', 'ChatController@index'); // メッセージ一覧を取得
-    Route::post('ajax/chat', 'ChatController@create'); // チャット登録
+    Route::get('chat', 'ChatController@index'); // メッセージ一覧を取得
+    Route::post('chat', 'ChatController@create'); // チャット登録
 
-    Route::get('ajax/room', 'RoomController@index'); //チャットルーム取得
-    Route::post('ajax/room', 'RoomController@create'); //チャットルーム作成
-    Route::post('ajax/roomHistry', 'RoomController@histry'); //入室済みチャットルーム取得
+    Route::get('room', 'RoomController@index'); //チャットルーム取得
+    Route::post('room', 'RoomController@create'); //チャットルーム作成
+    Route::post('roomHistry', 'RoomController@histry'); //入室済みチャットルーム取得
 
-    Route::post('ajax/enterroom', 'EnterRoom'); //チャットルーム入室処理
-    Route::post('ajax/isEntering', 'isEntering');  //チャットルーム入室確認
-    Route::post('ajax/roomlimit', 'CheckRoomLimit');   //チャットルームの制限時間確認
-    Route::post('ajax/roomauth', 'RoomAuth');   //チャットルームのパスワード認証
+    Route::post('enterroom', 'EnterRoom'); //チャットルーム入室処理
+    Route::post('isEntering', 'isEntering');  //チャットルーム入室確認
+    Route::post('roomlimit', 'CheckRoomLimit');   //チャットルームの制限時間確認
+    Route::post('roomauth', 'RoomAuth');   //チャットルームのパスワード認証
 });
 
 Route::get('/{any?}', function () {
