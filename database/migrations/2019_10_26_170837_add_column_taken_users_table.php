308903ef96f8c54e8c308a7204363c354b1f1e3b
nyan <apple@gmail.com>
2019-10-26 22:01:11 +0900

    fix migrate

--- database/migrations/2019_10_26_170837_add_column_taken_users_table.php
+++ database/migrations/2019_10_26_170837_add_column_taken_users_table.php
@@ -14,7 +14,7 @@ class AddColumnTakenUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('taken')->after('name');
+            $table->string('taken')->after('name')->nullable();
         });
     }
 
